55adbbefbc318eb8f87b0d24f5eca18052b5f387
crummy <crummynz@gmail.com>
2022-12-13 09:45:40 +1300

    Day 11 part 2

--- src/routes/day11/+page.svelte
+++ src/routes/day11/+page.svelte
@@ -5,58 +5,67 @@
   let useSampleData = true;
 
   interface Monkey {
-    items: number[]
-    operation: (i: number) => number
-    test: (i: number) => boolean
-    iftrue: number
-    iffalse: number
-    inspectedCount: number
+    items: number[];
+    operation: (i: number) => number;
+    divisibleTest: number;
+    iftrue: number;
+    iffalse: number;
+    inspectedCount: number;
   }
 
   $: data = useSampleData ? sample : input;
-  let monkeys: Monkey[]
+  let divideWorryByThree = true;
+  let monkeys: Monkey[];
   $: monkeys = data.split("\n\n").map(monkey => {
-    const [, items, operation, test, iftrue, iffalse] = monkey.split("\n")
+    const [, items, operation, test, iftrue, iffalse] = monkey.split("\n");
     const divisibleTest = Number(test.replace("  Test: divisible by ", ""));
     const ifTrueDestination = Number(iftrue.replace("    If true: throw to monkey ", ""));
     const ifFalseDestination = Number(iffalse.replace("    If false: throw to monkey ", ""));
-    const [operand, right] = operation.replace("  Operation: new = old ", "").split(" ")
+    const [operand, right] = operation.replace("  Operation: new = old ", "").split(" ");
     const oper = (val: number) => {
       const r = right === "old" ? val : Number(right);
       return operand === "+" ? val + r : val * r;
-    }
+    };
     return {
       items: items.replace("  Starting items: ", "").split(",").map(item => Number(item)),
       operation: oper,
-      test: (val) => val % divisibleTest === 0,
+      divisibleTest,
       iftrue: ifTrueDestination,
       iffalse: ifFalseDestination,
       inspectedCount: 0
-    }
+    };
   });
+  $: commonMultiplicand = monkeys.map(m => m.divisibleTest).reduce((a, b) => a * b);
   let steps = 0;
-  $: highInspectionMonkeys = monkeys.map(m => m.inspectedCount).sort((a, b) => b - a)
+  $: highInspectionMonkeys = monkeys.map(m => m.inspectedCount).sort((a, b) => b - a);
 
   function step() {
-    steps++
+    steps++;
     for (let monkey of monkeys) {
       while (monkey.items.length > 0) {
         let item = monkey.items.shift();
         monkey.inspectedCount++;
         item = monkey.operation(item);
-        item = Math.floor(item / 3)
-        if (monkey.test(item)) {
+        if (divideWorryByThree) {
+          item = Math.floor(item / 3);
+        } else {
+          item = item % commonMultiplicand;
+        }
+        if (item % monkey.divisibleTest === 0) {
           monkeys[monkey.iftrue].items.push(item);
         } else {
           monkeys[monkey.iffalse].items.push(item);
         }
       }
     }
-    monkeys = monkeys
+    monkeys = monkeys;
   }
 </script>
 
 <style>
+    .controls {
+        position: relative;
+    }
 </style>
 
 <h1>Day 11</h1>
@@ -64,7 +73,16 @@
   <input type="checkbox" bind:checked={useSampleData} />
   Use sample data
 </label>
-<button on:click={step}>Step {steps}</button>
+<div class="controls">
+  <p>Steps: {steps}</p>
+  <button on:click={step}>Step +1</button>
+  <button on:click={() => { for (let i = 0; i < 20; ++i) step()}}>Step +20</button>
+  <button on:click={() => { for (let i = 0; i < 10000; ++i) step()}}>Step +10000</button>
+  <label class="divide-by-three">
+    <input type="checkbox" bind:checked={divideWorryByThree} />
+    Divide worry by 3
+  </label>
+</div>
 <table>
   {#each monkeys as monkey, i}
     <tr>
